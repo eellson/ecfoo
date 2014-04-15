27709f7768e2741bd4834f0dce78b986adc0b018
Ed <ed@mintdigital.com>
2014-04-15 15:20:57 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecfoo::Application.config.secret_key_base = 'c1733bb61c32a718d2e232841ef60bedaf10d773af3140f3c7c0e362e17a5775e3275c23cbd017d9051e205f6686517faac12d0788dfde1be75749ad32a16f5c'
